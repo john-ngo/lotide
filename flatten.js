ca80775937de8b4260202c7bf5b998117ecdac30
Xuan Duong Ngo <ngoxd97@gmail.com>
2023-01-11 12:13:09 -0700

    added flatten function

---
+++ flatten.js
@@ -0,0 +1,35 @@
+const eqArrays = function(actual, expected) {
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const flatten = function(array) {
+  const newArray = [];
+
+  for (const el of array) {
+    if (Array.isArray(el)) {
+      for (const e of el) {
+        newArray.push(e);
+      }
+    } else {
+      newArray.push(el);
+    }
+  }
+
+  return newArray;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
