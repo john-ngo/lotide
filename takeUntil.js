d7fce1d8151b5ba25ff18b980f7c586b04300562
Xuan Duong Ngo <ngoxd97@gmail.com>
2023-01-12 18:30:48 -0700

    added takeUntil function

---
+++ takeUntil.js
@@ -0,0 +1,42 @@
+const eqArrays = function(actual, expected) {
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const takeUntil = function(array, callback) {
+  // ...
+  const newArray = [];
+  
+  for (const element of array) {
+    if (callback(element)) {
+      return newArray;
+    }
+
+    newArray.push(element);
+  }
+
+  return newArray;
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual([1, 2, 5, 7, 2], results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(["I've", 'been', 'to', 'Hollywood'], results2)
